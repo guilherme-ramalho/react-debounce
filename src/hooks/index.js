9700536767ac56e1ddf948d1e85a7ecac27be878
guilherme-ramalho <guilherme-ramalho@outlook.com>
2020-07-07 16:38:19 -0300

    Adding default value for hook timeout

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
 
-export function useDebounce(search, timeout) {
+export function useDebounce(search, timeout = 1000) {
   const [debouncedSearch, setDebouncedSearch] = useState(search);
 
   useEffect(() => {
